e2a054e33a19b08e901eb1264477e2a1e020e291
lanthora <lanthora@outlook.com>
2021-02-25 20:55:58 +0800

    封装插入内核模块的系统调用

--- service/syscall.c
+++ service/syscall.c
@@ -1,7 +1,12 @@
 #include "syscall.h"
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int check_sys_call_table_addr(unsigned long *sys_call_table) {
@@ -15,11 +20,44 @@ int check_sys_call_table_addr(unsigned long *sys_call_table) {
     return retval;
   }
   while (fgets(line, sizeof(line), kallsyms)) {
-    if (!strstr(line, " sys_call_table"))
-      continue;
+    if (!strstr(line, " sys_call_table")) continue;
     sscanf(line, "%lx", sys_call_table);
     retval = 0;
   }
   fclose(kallsyms);
   return retval;
 }
+
+int insmod(const char *filename) {
+  int error = -1;
+  struct stat st;
+  int fd;
+  void *image;
+  int image_size = 0;
+
+  fd = open(filename, O_RDONLY);
+  if (fd < 0) goto out;
+
+  error = fstat(fd, &st);
+  if (error) goto closefd;
+
+  image_size = st.st_size;
+  image = malloc(image_size);
+  if (!image) goto closefd;
+
+  error = read(fd, image, image_size);
+  if (error < 0) goto freemem;
+
+  error = syscall(__NR_init_module, image, image_size, "");
+  if (error) goto freemem;
+
+  error = 0;
+freemem:
+  free(image);
+closefd:
+  close(fd);
+out:
+  return error;
+}
+
+int rmmod(const char *modulename) {}
--- service/syscall.h
+++ service/syscall.h
@@ -2,5 +2,7 @@
 #define HACKERNEL_SYSCALL
 
 int check_sys_call_table_addr(unsigned long *sys_call_table);
+int insmod(const char *filename);
+int rmmod(const char *modulename);
 
 #endif
