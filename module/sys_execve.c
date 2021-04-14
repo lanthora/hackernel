754da04ae529db67fb108ec98d39ef415763c670
lanthora <lanthora@outlook.com>
2021-04-15 00:34:38 +0800

    打印参数

--- module/sys_execve.c
+++ module/sys_execve.c
@@ -5,16 +5,35 @@
 
 static sys_call_ptr_t real_execve = NULL;
 
+static int custom_execve_pretty(const char *pathname, const char *const *argv,
+			const char *const *envp)
+{
+	int i;
+	int argv_cout = count_strings(argv);
+	int envp_cout = count_strings(envp);
+
+	printk("hackernel: filename=%s", pathname);
+	printk("hackernel: argv count=%d", argv_cout);
+	printk("hackernel: envp count=%d", envp_cout);
+
+	for (i = 0; i < argv_cout; ++i) {
+		printk("hackernel: argv[%d]=%s", i, argv[i]);
+	}
+
+	for (i = 0; i < envp_cout; ++i) {
+		printk("hackernel: envp[%d]=%s", i, envp[i]);
+	}
+	return 0;
+}
+
 asmlinkage u64 custom_execve(const struct pt_regs *regs)
 {
 	const char *pathname = (const char *)regs->di;
 	// 这种指针最后一个指针指向一个空字符串 "\0"
 	const char *const *argv = (const char *const *)regs->si;
 	const char *const *envp = (const char *const *)regs->dx;
-
-	printk("hackernel: filename=%s", pathname);
-	printk("hackernel: argv count=%d", count_strings(argv));
-	printk("hackernel: envp count=%d", count_strings(envp));
+	if(custom_execve_pretty(pathname, argv, envp))
+		return -EPERM;
 	return real_execve(regs);
 }
 
