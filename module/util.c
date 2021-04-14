fbfebc93db7956b02f77f6479e1579b3dbafcd9b
lanthora <lanthora@outlook.com>
2021-04-14 23:48:22 +0800

    修改参数个数判断条件

--- module/util.c
+++ module/util.c
@@ -3,7 +3,7 @@ uint8_t argc(char **argv)
 {
 	uint8_t count = 0;
 	char *current_argv = *argv;
-	while (*current_argv && count != (uint8_t)(-1)) {
+	while (current_argv && count != (uint8_t)(-1)) {
 		++count;
 	}
 	return count;
