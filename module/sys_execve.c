1cde95b943e246116c6c44a78429fcbfab043553
lanthora <lanthora@outlook.com>
2021-04-18 18:31:57 +0800

    移除进入系统调用时的原子操作
    这个操作在使用vscode cpptools时会引发死锁，
    具体原因未知

--- module/sys_execve.c
+++ module/sys_execve.c
@@ -10,31 +10,19 @@ static sys_call_ptr_t real_execve = NULL;
 static int sys_evecve(char __user *pathname, char __user *__user *argv,
 		      char __user *__user *envp)
 {
-	char *path = NULL, *params = NULL;
+	char *cmd = NULL;
 	int error = 0;
 
-	path = kzalloc(PATH_MAX, GFP_KERNEL);
-	if (!path) {
-		printk(KERN_ERR "hackernel: kmalloc path failed!\n");
+	cmd = kzalloc(ARG_MAX, GFP_KERNEL);
+	if (!cmd) {
 		error = -1;
 		goto out;
 	}
-
-	parse_pathname(pathname, path, PATH_MAX);
-	printk(KERN_INFO "hackernel: path=%s\n", path);
-
-	params = kzalloc(ARG_MAX, GFP_KERNEL);
-	if (!params) {
-		printk(KERN_ERR "hackernel: kmalloc params failed!\n");
-		error = -1;
-		goto out;
-	}
-	parse_argv((const char *const *)argv, params, ARG_MAX);
-	printk(KERN_INFO "hackernel: cmd=%s\n", params);
+	parse_argv((const char *const *)argv, cmd, ARG_MAX);
+	printk(KERN_INFO "hackernel: cmd=%s\n", cmd);
 
 out:
-	kfree(path);
-	kfree(params);
+	kfree(cmd);
 	return error;
 }
 
@@ -44,15 +32,9 @@ asmlinkage u64 raw_sys_execve(struct pt_regs *regs)
 	char **argv = (char **)regs->si;
 	char **envp = (char **)regs->dx;
 
-	static atomic_t running_execve_cnt = ATOMIC_INIT(0);
-	int cnt = atomic_inc_return(&running_execve_cnt);
-	if (cnt > 1) {
-		restore_execve();
-	}
 	if (sys_evecve(pathname, argv, envp)) {
 		restore_execve();
 	}
-	atomic_dec(&running_execve_cnt);
 	return real_execve(regs);
 }
 
