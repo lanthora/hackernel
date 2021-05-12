63101c05bd393a83ee29aa77585f623e720d7e5e
lanthora <lanthora@outlook.com>
2021-05-12 22:27:55 +0800

    更新获取execve参数的实现

--- module/util.c
+++ module/util.c
@@ -7,30 +7,29 @@
 #include <uapi/asm-generic/errno-base.h>
 #include <uapi/linux/binfmts.h>
 
-static const char __user *get_user_arg_ptr(char __user *__user *argv, int nr)
-{
-	const char __user *native;
-	if (get_user(native, argv + nr))
-		return ERR_PTR(-EFAULT);
-
-	return native;
-}
-
 static int count(char __user *__user *argv, int max)
 {
 	int i = 0;
+	int error = 0;
+	char *p;
 	if (!argv) {
-		return i;
+		goto failed;
 	}
 
 	for (;;) {
-		const char __user *p = get_user_arg_ptr(argv, i);
-		if (!p)
+		error = get_user(p, argv + i);
+		if (error) {
+			goto failed;
+		}
+		if (!p) {
 			break;
+		}
 		++i;
 	}
-
 	return i;
+
+failed:
+	return 0;
 }
 
 int parse_pathname(const char __user *pathname, char *path, long size)
@@ -48,29 +47,47 @@ int parse_argv(const char __user *const __user *argv, char *params, long size)
 	int retval = -1;
 	int argc;
 
-	argc = count((char **)argv, MAX_ARG_STRINGS);
-	p = kmalloc((argc + 1) * sizeof(char *), GFP_KERNEL);
+	argc = count((char **)argv, BINPRM_BUF_SIZE);
+	if (!argc) {
+		goto out;
+	}
 
-	lack = copy_from_user(p, argv, argc * sizeof(char *));
+	p = kmalloc(argc * sizeof(void *), GFP_KERNEL);
+
+	if (!p) {
+		goto out;
+	}
+
+	lack = copy_from_user(p, argv, argc * sizeof(void *));
 	if (lack) {
 		goto out;
 	}
 
 	len = 0, cursor = params;
-	for (idx = 0; p[idx]; ++idx) {
+	for (idx = 0; idx < argc; ++idx) {
 		remain = size - (cursor - params);
 		if (remain <= 0) {
 			break;
 		}
+
 		len = strnlen_user(p[idx], remain);
+		if (!len) {
+			break;
+		}
+
 		lack = copy_from_user(cursor, p[idx], len);
 		if (lack) {
 			break;
 		}
+
 		cursor += len;
-		*(cursor - 1) = ' ';
+		if (cursor > params) {
+			*(cursor - 1) = ' ';
+		}
 	}
-	*(cursor - 1) = '\0';
+	if (cursor > params) {
+		*(cursor - 1) = '\0';
+	}
 	retval = 0;
 out:
 	kfree(p);
