448dea8fd3547c9ac37eeccccf429a82af47af19
lanthora <lanthora@outlook.com>
2021-05-12 22:30:09 +0800

    更新变量命名

--- module/sys_execve.c
+++ module/sys_execve.c
@@ -2,23 +2,27 @@
 #include "syscall.h"
 #include "util.h"
 #include <linux/binfmts.h>
-#include <linux/syscalls.h>
+#include <linux/gfp.h>
+#include <linux/slab.h>
 
-static sys_call_ptr_t real_execve = NULL;
+static sys_call_ptr_t __x64_sys_execve = NULL;
 
 // 系统调用的参数与内核源码中 include/linux/syscalls.h 中的声明保持一致
-static int sys_evecve(char __user *pathname, char __user *__user *argv,
-		      char __user *__user *envp)
+static int sys_execve_hook(char __user *pathname, char __user *__user *argv,
+			   char __user *__user *envp)
 {
 	char *cmd = NULL;
-	int error = 0;
+	int error = -1;
 
-	cmd = kzalloc(ARG_MAX, GFP_KERNEL);
+	cmd = kzalloc(MAX_ARG_STRLEN, GFP_KERNEL);
 	if (!cmd) {
 		error = -1;
 		goto out;
 	}
-	parse_argv((const char *const *)argv, cmd, ARG_MAX);
+	error = parse_argv((const char *const *)argv, cmd, MAX_ARG_STRLEN);
+	if (error) {
+		goto out;
+	}
 	printk(KERN_INFO "hackernel: cmd=%s\n", cmd);
 
 out:
@@ -26,15 +30,15 @@ out:
 	return error;
 }
 
-asmlinkage u64 raw_sys_execve(struct pt_regs *regs)
+asmlinkage u64 sys_execve_wrapper(struct pt_regs *regs)
 {
 	char *pathname = (char *)regs->di;
 	char **argv = (char **)regs->si;
 	char **envp = (char **)regs->dx;
 
-	if (sys_evecve(pathname, argv, envp)) {
+	if (sys_execve_hook(pathname, argv, envp)) {
 	}
-	return real_execve(regs);
+	return __x64_sys_execve(regs);
 }
 
 int replace_execve(void)
@@ -44,28 +48,28 @@ int replace_execve(void)
 		return -1;
 	}
 
-	if (real_execve) {
+	if (__x64_sys_execve) {
 		printk(KERN_ERR "hackernel: execve doulbe init\n");
 		return 0;
 	}
 
-	real_execve = g_sys_call_table[__NR_execve];
+	__x64_sys_execve = g_sys_call_table[__NR_execve];
 	disable_write_protection();
-	g_sys_call_table[__NR_execve] = &raw_sys_execve;
+	g_sys_call_table[__NR_execve] = &sys_execve_wrapper;
 	enable_write_protection();
 	return 0;
 }
 
 int restore_execve(void)
 {
-	if (!g_sys_call_table || !real_execve) {
+	if (!g_sys_call_table || !__x64_sys_execve) {
 		printk(KERN_WARNING "hackernel: restore failed\n");
 		return 0;
 	}
 
 	disable_write_protection();
-	g_sys_call_table[__NR_execve] = real_execve;
+	g_sys_call_table[__NR_execve] = __x64_sys_execve;
 	enable_write_protection();
-	real_execve = NULL;
+	__x64_sys_execve = NULL;
 	return 0;
 }
