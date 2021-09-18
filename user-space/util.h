be64d07602ca2060cc9ec7778c1af0d2ee4f6015
lanthora <lanthora@outlook.com>
2021-09-18 14:41:16 +0000

    日志添加时间标记
    我失忆了吗?明明记得服务有问题,再打开看日志和服务开启时间又没有问题.
    日志加上时间戳,再出了这个问题方便定位

--- user-space/util.h
+++ user-space/util.h
@@ -2,10 +2,15 @@
 #define HACKERNEL_UTIL_H
 
 #include <stdio.h>
+#include <time.h>
 
 #define LOG(fmt, arg...)                                                       \
   do {                                                                         \
-    printf("%s:%d " fmt "\n", __FILE__, __LINE__, ##arg);                      \
+    time_t now = time(NULL);                                                   \
+    struct tm *t = localtime(&now);                                            \
+    printf("[%d-%d-%d %d:%d:%d] [%s:%d] " fmt "\n", t->tm_year + 1900,         \
+           t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec,        \
+           __FILE__, __LINE__, ##arg);                                         \
     fflush(stdout);                                                            \
   } while (0)
 
