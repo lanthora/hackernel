a0c76a6ea3615744b402e22f7b8570c53f627dd0
lanthora <lanthora@outlook.com>
2021-09-18 14:55:29 +0000

    美化时间输出格式

--- user-space/util.h
+++ user-space/util.h
@@ -8,9 +8,9 @@
   do {                                                                         \
     time_t now = time(NULL);                                                   \
     struct tm *t = localtime(&now);                                            \
-    printf("[%d-%d-%d %d:%d:%d] [%s:%d] " fmt "\n", t->tm_year + 1900,         \
-           t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec,        \
-           __FILE__, __LINE__, ##arg);                                         \
+    printf("[%04d-%02d-%02d %02d:%02d:%02d] [%s:%d] " fmt "\n",                \
+           t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour,           \
+           t->tm_min, t->tm_sec, __FILE__, __LINE__, ##arg);                   \
     fflush(stdout);                                                            \
   } while (0)
 
