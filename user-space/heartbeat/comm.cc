295750930c68b768c13f70353f0bfcec5166fd60
lanthora <lanthora@outlook.com>
2021-12-11 20:43:28 +0800

    调整文件名

--- user-space/heartbeat/comm.cc
+++ user-space/heartbeat/comm.cc
@@ -42,4 +42,15 @@ int HeartbeatWait() {
     return HeartbeatHelper(1);
 }
 
+int HeartbeatHandler(struct nl_cache_ops *unused, struct genl_cmd *genl_cmd, struct genl_info *genl_info, void *arg) {
+    int code = nla_get_s32(genl_info->attrs[HANDSHAKE_A_STATUS_CODE]);
+    if (code) {
+        LOG("handshake response code=[%d]", code);
+        LOG("handshake failed. exit");
+        exit(1);
+    }
+
+    return 0;
+}
+
 }; // namespace hackernel
