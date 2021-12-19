2c80751d61ca3643bc067f7ff93bab9eab75b0a8
lanthora <lanthora@outlook.com>
2021-12-19 13:39:20 +0800

    移除测试代码

--- user-space/ipc/server.cc
+++ user-space/ipc/server.cc
@@ -179,97 +179,9 @@ Session IpcServer::NewUserSession() {
     return id_;
 }
 
-#define FILE_PROTECT 0
-#define NET_PROTECT 0
-
-int IpcTest() {
-    LOG("IpcTest Start");
-
-#if FILE_PROTECT
-    FileProtectEnable();
-    FileProtectSet("/etc/fstab", FLAG_FILE_READ_ONLY);
-    FileProtectSet("/boot/grub/grub.cfg", FLAG_FILE_ALL_DISABLE);
-    FileProtectSet("/etc/host.conf", FLAG_FILE_READ_ONLY);
-#endif
-
-#if NET_PROTECT
-    NetProtectEnable();
-
-    NetPolicy policy;
-    policy.addr.src.begin = ntohl(inet_addr("0.0.0.0"));
-    policy.addr.src.end = ntohl(inet_addr("255.255.255.255"));
-    policy.addr.dst.begin = ntohl(inet_addr("0.0.0.0"));
-    policy.addr.dst.end = ntohl(inet_addr("255.255.255.255"));
-    policy.protocol.begin = 6;
-    policy.protocol.end = 6;
-
-    // allow ssh
-    policy.port.src.begin = 0;
-    policy.port.src.end = UINT16_MAX;
-    policy.port.dst.begin = 22;
-    policy.port.dst.end = 22;
-
-    policy.id = 0;
-    policy.priority = 0;
-    policy.flags = FLAG_NET_INBOUND;
-    policy.response = NET_POLICY_ACCEPT;
-    NetPolicyInsert(&policy);
-
-    policy.port.src.begin = 22;
-    policy.port.src.end = 22;
-    policy.port.dst.begin = 0;
-    policy.port.dst.end = UINT16_MAX;
-    policy.flags = FLAG_NET_OUTBOUND;
-    policy.response = NET_POLICY_ACCEPT;
-    NetPolicyInsert(&policy);
-
-    // allow tcp header
-    policy.port.src.begin = 0;
-    policy.port.src.end = UINT16_MAX;
-    policy.port.dst.begin = 0;
-    policy.port.dst.end = UINT16_MAX;
-    policy.id = 1;
-    policy.priority = 1;
-    policy.flags = FLAG_NET_OUTBOUND | FLAG_NET_ONLY_ALLOW_TCP_HEADER;
-    policy.response = NET_POLICY_ACCEPT;
-    NetPolicyInsert(&policy);
-
-    // allow localhost
-    policy.addr.src.begin = ntohl(inet_addr("127.0.0.1"));
-    policy.addr.src.end = ntohl(inet_addr("127.0.0.1"));
-    policy.addr.dst.begin = ntohl(inet_addr("127.0.0.1"));
-    policy.addr.dst.end = ntohl(inet_addr("127.0.0.1"));
-    policy.flags = FLAG_NET_INBOUND | FLAG_NET_OUTBOUND;
-    NetPolicyInsert(&policy);
-
-    // docker
-    policy.addr.src.begin = ntohl(inet_addr("172.17.0.0"));
-    policy.addr.src.end = ntohl(inet_addr("172.17.255.255"));
-    policy.addr.dst.begin = ntohl(inet_addr("172.17.0.0"));
-    policy.addr.dst.end = ntohl(inet_addr("172.17.255.255"));
-    policy.flags = FLAG_NET_INBOUND | FLAG_NET_OUTBOUND;
-    NetPolicyInsert(&policy);
-
-    // disable others
-    policy.addr.src.begin = ntohl(inet_addr("0.0.0.0"));
-    policy.addr.src.end = ntohl(inet_addr("255.255.255.255"));
-    policy.addr.dst.begin = ntohl(inet_addr("0.0.0.0"));
-    policy.addr.dst.end = ntohl(inet_addr("255.255.255.255"));
-    policy.id = 2;
-    policy.priority = 2;
-    policy.flags = FLAG_NET_OUTBOUND;
-    policy.response = NET_POLICY_DROP;
-    NetPolicyInsert(&policy);
-#endif
-
-    return 0;
-}
-
 int IpcWait() {
     IpcServer::GetInstance().Init();
-    IpcTest();
     IpcServer::GetInstance().StartWait();
-
     return 0;
 }
 
