6a3b045e84a6363c86c75c090879ec10b06ce6cf
lanthora <lanthora@outlook.com>
2021-12-25 17:51:25 +0800

    刷新流错误流

--- user-space/hackernel/util.h
+++ user-space/hackernel/util.h
@@ -39,7 +39,7 @@ EXTERN_C_BEGIN
         struct tm *t = localtime(&now);                                                                                \
         fprintf(stderr, "[%04d-%02d-%02d %02d:%02d:%02d] [%s:%d] " fmt "\n", t->tm_year + 1900, t->tm_mon + 1,         \
                 t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec, __FILE__, __LINE__, ##arg);                              \
-        fflush(stdout);                                                                                                \
+        fflush(stderr);                                                                                                \
     } while (0)
 
 #define ThreadNameUpdate(name)                                                                                         \
