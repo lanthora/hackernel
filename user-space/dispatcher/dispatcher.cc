2ba4651ea397141a6cdd2fc498647188412bae50
lanthora <lanthora@outlook.com>
2022-01-11 07:46:06 -0500

    优化dispatcher CPU占用

--- user-space/dispatcher/dispatcher.cc
+++ user-space/dispatcher/dispatcher.cc
@@ -3,15 +3,45 @@
 #include "dispatcher/handler.h"
 #include "hackernel/broadcaster.h"
 #include "hackernel/util.h"
+#include <nlohmann/json.hpp>
+#include <unordered_set>
 
 namespace hackernel {
 
 static std::shared_ptr<Receiver> dispatcher = nullptr;
+static std::unordered_set<std::string> enabled_type_set;
+
+static void DispatcherFilterInit() {
+    enabled_type_set.clear();
+    enabled_type_set.insert("user::proc::enable");
+    enabled_type_set.insert("user::proc::disable");
+    enabled_type_set.insert("user::file::enable");
+    enabled_type_set.insert("user::file::disable");
+    enabled_type_set.insert("user::file::set");
+    enabled_type_set.insert("user::net::enable");
+    enabled_type_set.insert("user::net::disable");
+    enabled_type_set.insert("user::net::insert");
+    enabled_type_set.insert("user::net::delete");
+}
+
+static bool DispatcherFilter(const std::string &msg) {
+    nlohmann::json doc = nlohmann::json::parse(msg);
+    if (!doc.is_object())
+        return true;
+    if (!doc["type"].is_string())
+        return true;
+    if (!enabled_type_set.contains(doc["type"]))
+        return true;
+    return false;
+}
 
 int DispatcherWait() {
     ThreadNameUpdate("dispatcher");
+    DispatcherFilterInit();
+
     dispatcher = std::make_shared<Receiver>();
 
+    dispatcher->AddHandler(DispatcherFilter);
     dispatcher->AddHandler(UserProcEnable);
     dispatcher->AddHandler(UserProcDisable);
     dispatcher->AddHandler(UserFileEnable);
