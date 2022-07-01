2329af666d59f804c0614af22f7edcff99a16652
lanthora <lanthora@outlook.com>
2022-07-01 15:21:57 +0000

    保存时格式化代码

--- kernel-space/watchdog/watchdog.c
+++ kernel-space/watchdog/watchdog.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-#include "watchdog/api.h"
 #include "hackernel/log.h"
+#include "watchdog/api.h"
 #include <linux/kthread.h>
 
 static int watchdog_kthread(void *data)
