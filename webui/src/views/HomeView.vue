56a64e2edd39c63845d7d9e2d88e5238fa02a51c
lanthora <lanthora@outlook.com>
2023-07-22 05:25:09 +0000

    添加前端页面
    1. 删除不使用的示例代码
    2. 添加加载页面
    3. 添加登录页面
    4. 添加策略卡片和主页

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -1,9 +1,44 @@
+<template>
+  <div>
+    <div class="flex-container">
+      <PolicyCard v-bind="procInfo"></PolicyCard>
+      <PolicyCard v-bind="fileInfo"></PolicyCard>
+      <PolicyCard v-bind="netInfo"></PolicyCard>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import PolicyCard from '../components/PolicyCard.vue'; 
+
+const procInfo = {
+  title: "进程防护",
+  status: "启用",
+  policyCount: 1,
+  unreadCount: 2
+};
+
+const fileInfo = {
+  title: "文件防护",
+  status: "启用",
+  policyCount: 1,
+  unreadCount: 2
+};
+
+const netInfo = {
+  title: "网络防护",
+  status: "启用",
+  policyCount: 1,
+  unreadCount: 2
+};
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.flex-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
